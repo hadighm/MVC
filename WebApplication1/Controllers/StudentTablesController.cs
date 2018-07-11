fb236e6e9b9ee1f7c25c4c76dc969612fddaa54f
hadi <hadigotbi@gmail.com>
2018-07-11 07:01:36 +0430

    Add project files.

---
+++ WebApplication1/Controllers/StudentTablesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentTablesController : Controller
+    {
+        private StudentDBEntities db = new StudentDBEntities();
+
+        // GET: StudentTables
+        public ActionResult Index()
+        {
+            return View(db.StudentTables.ToList());
+        }
+
+        // GET: StudentTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentTable studentTable = db.StudentTables.Find(id);
+            if (studentTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentTable);
+        }
+
+        // GET: StudentTables/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentTables/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StudentID,FirstName,LastName,Score")] StudentTable studentTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StudentTables.Add(studentTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(studentTable);
+        }
+
+        // GET: StudentTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentTable studentTable = db.StudentTables.Find(id);
+            if (studentTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentTable);
+        }
+
+        // POST: StudentTables/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StudentID,FirstName,LastName,Score")] StudentTable studentTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studentTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(studentTable);
+        }
+
+        // GET: StudentTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentTable studentTable = db.StudentTables.Find(id);
+            if (studentTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentTable);
+        }
+
+        // POST: StudentTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            StudentTable studentTable = db.StudentTables.Find(id);
+            db.StudentTables.Remove(studentTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
