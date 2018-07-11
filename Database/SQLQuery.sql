28ccacc5dfabb14432e2b5918ab06f302780cfff
hadi <hadigotbi@gmail.com>
2018-07-11 14:53:10 +0430

    Add database sql code

---
+++ Database/SQLQuery.sql
@@ -0,0 +1,8 @@
+use master
+create database StudentDB
+use StudentDB
+create Table StudentTable (
+StudentID int primary key not null,
+FirstName varchar(20) not null,
+LastName varchar(20)not null,
+Score int not null ) 
